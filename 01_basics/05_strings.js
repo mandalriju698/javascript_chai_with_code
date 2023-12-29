7a0f479e549bbdbaea6c6347a9076f11ff06e3a6
mandalriju698 <88059569+mandalriju698@users.noreply.github.com>
2023-12-29 14:49:17 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name="riju"
+const repoCount=50
+
+//console.log(name+repoCount+"value");
+
+console.log(`hello my name is ${name} anr my repoCount is ${repoCount}`);
+
+
+const gameName=new String('rijusc');
