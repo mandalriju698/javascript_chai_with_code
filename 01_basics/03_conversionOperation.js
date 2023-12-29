0d2d95023d3fcbde1550c262ad6a5ee6f1003bc6
mandalriju698 <88059569+mandalriju698@users.noreply.github.com>
2023-12-29 07:17:16 +0000

    conversionOperaration

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score ="33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber=Number(score)
+
+// console.log(typeof valueNumber);
+
+
+// ****************operations************
+let value=3
+let negValue= -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+
+let str1="hello"
+let str2="hitesh"
+
+let str3=str1+str2;
+console.log(str3);
