a402febb62503217b46720b6bd739943b9e94c74
mandalriju698 <88059569+mandalriju698@users.noreply.github.com>
2023-12-29 05:50:45 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as new nersion
+
+//alert(9+6);  //we are using node js not browser
+
+
+
+//code readiblity should be high
+
+let name="riju";
+let age=18
+let isLoggedIn=false;
+let state;
+
+//number => 2 to power 53 
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+// symbol => unique
+
+
+//boject
+
+
+console.log(typeof "hitesh")
