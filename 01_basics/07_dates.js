f43468bdec455d733736adfd8320f8b5289f35de
mandalriju698 <88059569+mandalriju698@users.noreply.github.com>
2023-12-30 08:16:47 +0000

    date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,12 @@
+//Dates
+
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+//time stamp
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
